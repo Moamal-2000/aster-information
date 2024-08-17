f91a432bd528f3c48f9672a390e314ce3cefa7fc
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-17 08:30:28 +0300

    chore: write header's structure

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,25 @@
+import { Link } from "react-router-dom";
+import SvgIcon from "../Shared/MiniComponents/SvgIcon";
 import s from "./Header.module.scss";
 
 const Header = () => {
-  return <header className={s.header}>Header</header>;
+  return (
+    <header className={s.header}>
+      <Link to="/">
+        <SvgIcon name="mainIcon" />
+        <span>ASTER INFORMATION</span>
+      </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/technology">Technology</Link>
+            <Link to="/about">About</Link>
+            <Link to="/careers">Careers</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 export default Header;
