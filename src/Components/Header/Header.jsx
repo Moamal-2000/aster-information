df110f9d63f5a41593188610ed00bfac78faac32
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-17 09:36:56 +0300

    feat: done of header styling & responsiveness

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,24 +5,26 @@ import s from "./Header.module.scss";
 const Header = () => {
   return (
     <header className={s.header}>
-      <Link to="/">
-        <SvgIcon name="mainIcon" />
-        <span>ASTER INFORMATION</span>
-      </Link>
+      <div className="container">
+        <Link to="/">
+          <SvgIcon name="mainIcon" />
+          <span>ASTER INFORMATION</span>
+        </Link>
 
-      <nav className={s.nav}>
-        <ul>
-          <li>
-            <NavLink to="/technology">Technology</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/careers">Careers</NavLink>
-          </li>
-        </ul>
-      </nav>
+        <nav className={s.nav}>
+          <ul>
+            <li>
+              <NavLink to="/technology">Technology</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/careers">Careers</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
