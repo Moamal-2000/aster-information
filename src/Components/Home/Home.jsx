4de8c32b660c23712f190b2d7d043d4e072a6fc5
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-17 13:04:07 +0300

    chore: initial home hero section styles

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,20 @@
-import s from "./Home.module.scss"
+import homeBgBottom from "../../Assets/home-bg-bottom.png";
+import homeBgTop from "../../Assets/home-bg-top.png";
+import s from "./Home.module.scss";
+import HomeHeroSection from "./HomeHeroSection/HomeHeroSection";
 
 const Home = () => {
   return (
-    <main className={s.homePage}>Home</main>
-  )
-}
-export default Home
+    <main className={s.homePage}>
+      <div className={s.backgroundImage}>
+        <img src={homeBgTop} className={s.top} alt="" />
+        <img src={homeBgBottom} className={s.bottom} alt="" />
+      </div>
+
+      <div className="container">
+        <HomeHeroSection />
+      </div>
+    </main>
+  );
+};
+export default Home;
---
+++ src/Components/Home/HomeHeroSection/HomeHeroSection.jsx
@@ -0,0 +1,11 @@
+import s from "./HomeHeroSection.module.scss";
+
+const HomeHeroSection = () => {
+  return (
+    <div className={s.heroSection}>
+      <h1>POWERING BUSINESS GROWTH WITH AI AND DATA MANAGEMENT</h1>
+      <h2>Unlock the Power of Data with Aster Information Limited</h2>
+    </div>
+  );
+};
+export default HomeHeroSection;
