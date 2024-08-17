743216d7e01fb715111775091c0956d750dbd3c3
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-17 11:15:58 +0300

    style: test mobile nav menu transition

--- src/Components/Header/MobileNav/MobileNav.jsx
+++ src/Components/Header/MobileNav/MobileNav.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import s from "./MobileNav.module.scss";
 
 const MobileNav = () => {
+  const { isMobileNavActive } = useSelector((state) => state.global);
+  const activeClass = isMobileNavActive ? s.active : "";
+
   return (
-    <nav className={s.nav}>
+    <nav className={`${s.nav} ${activeClass}`}>
       <ul>
         <li>
           <NavLink to="/technology">Technology</NavLink>
