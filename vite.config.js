8de94422060ab2425ec1b5bf373103abbea717ea
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-17 08:22:17 +0300

    feat: enable sourcemap & enable relative path to /src

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
 });
