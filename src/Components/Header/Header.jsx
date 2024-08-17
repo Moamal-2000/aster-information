7e0fac08f91a7dd91ec3be6f10e30a30f3be392d
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-17 10:08:33 +0300

    chore: spread nav icon to NavIcon component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
-import { updateGlobalState } from "../../Features/globalSlice";
 import SvgIcon from "../Shared/MiniComponents/SvgIcon";
 import s from "./Header.module.scss";
+import NavIcon from "./NavIcon/NavIcon";
 
 const Header = () => {
-  const { isMobileNavActive } = useSelector((state) => state.global);
-  const dispatch = useDispatch();
-  const activeClass = isMobileNavActive ? s.active : "";
-
-  function toggleMobileNav() {
-    dispatch(
-      updateGlobalState({ key: "isMobileNavActive", value: !isMobileNavActive })
-    );
-  }
   return (
     <header className={s.header}>
       <div className="container">
@@ -36,10 +26,7 @@ const Header = () => {
           </ul>
         </nav>
 
-        <i
-          className={`${s.navIcon} ${activeClass}`}
-          onClick={toggleMobileNav}
-        />
+        <NavIcon />
       </div>
     </header>
   );
---
+++ src/Components/Header/NavIcon/NavIcon.jsx
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { updateGlobalState } from "src/Features/globalSlice";
+import s from "./NavIcon.module.scss";
+
+const NavIcon = () => {
+  const { isMobileNavActive } = useSelector((state) => state.global);
+  const dispatch = useDispatch();
+  const activeClass = isMobileNavActive ? s.active : "";
+
+  function toggleMobileNav() {
+    dispatch(
+      updateGlobalState({ key: "isMobileNavActive", value: !isMobileNavActive })
+    );
+  }
+
+  return (
+    <i className={`${s.navIcon} ${activeClass}`} onClick={toggleMobileNav} />
+  );
+};
+export default NavIcon;
