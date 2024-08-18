f6f720e8adb00b73cd287196355a5e3e4e8b4d91
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-18 16:11:08 +0300

    feat: initiate Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,6 @@
+import s from "./Footer.module.scss";
+
+const Footer = () => {
+  return <footer className={s.footer}>Footer</footer>;
+};
+export default Footer;
--- src/Routes/RouteLayout.jsx
+++ src/Routes/RouteLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
 
 const RouteLayout = () => {
@@ -6,6 +7,7 @@ const RouteLayout = () => {
     <div className="App">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
