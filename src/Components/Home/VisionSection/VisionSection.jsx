ffe36b7c307e2c7a2349214ceaa9b65f68198d94
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-18 10:30:28 +0300

    feat: spread vision section content into VisionSectionContent component

--- src/Components/Home/VisionSection/VisionSection.jsx
+++ src/Components/Home/VisionSection/VisionSection.jsx
@@ -1,6 +1,7 @@
 import AnimatedLine from "../../Shared/MiniComponents/AnimatedLine/AnimatedLine";
 import AnimatedImage from "./AnimatedImage/AnimatedImage";
 import s from "./VisionSection.module.scss";
+import VisionSectionContent from "./VisionSectionContent/VisionSectionContent";
 
 const VisionSection = () => {
   return (
@@ -8,21 +9,7 @@ const VisionSection = () => {
       <div className="container">
         <div className={s.wrapper}>
           <AnimatedLine lineHeight="470px" yAxis="150px" animateTo="70px" />
-
-          <div className={s.content}>
-            <h2 className={s.title}>VISION</h2>
-            <p className={s.subTitle}>
-              Driving Innovation in Information Management
-            </p>
-            <p className={s.description}>
-              At Aster Information Limited, we're committed to revolutionizing
-              the way businesses manage data and IT solutions. By harnessing the
-              power of AI, we aim to deliver services that redefine efficiency
-              and innovation. We believe in a future where strategic information
-              management propels businesses to new heights, and we're at the
-              forefront of making that future a reality.
-            </p>
-          </div>
+          <VisionSectionContent />
         </div>
 
         <AnimatedImage />
---
+++ src/Components/Home/VisionSection/VisionSectionContent/VisionSectionContent.jsx
@@ -0,0 +1,21 @@
+import s from "./VisionSectionContent.module.scss";
+
+const VisionSectionContent = () => {
+  return (
+    <div className={s.content}>
+      <h2 className={s.title}>VISION</h2>
+
+      <p className={s.subTitle}>Driving Innovation in Information Management</p>
+
+      <p className={s.description}>
+        At Aster Information Limited, we're committed to revolutionizing the way
+        businesses manage data and IT solutions. By harnessing the power of AI,
+        we aim to deliver services that redefine efficiency and innovation. We
+        believe in a future where strategic information management propels
+        businesses to new heights, and we're at the forefront of making that
+        future a reality.
+      </p>
+    </div>
+  );
+};
+export default VisionSectionContent;
