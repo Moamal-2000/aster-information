9c5fdd8a0376d13b82d7018d6e4c7c09371bb5fe
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-18 16:29:11 +0300

    feat: build structure of Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,87 @@
+import { Link } from "react-router-dom";
+import {
+  facebookLogo,
+  instagramLogo,
+  linkedinLogo,
+  twitterLogo,
+} from "src/Assets/Images/Images";
 import s from "./Footer.module.scss";
 
 const Footer = () => {
-  return <footer className={s.footer}>Footer</footer>;
+  return (
+    <footer className={s.footer}>
+      <div className={s.footerDetails}>
+        <Link to="/">ASTER INFORMATION</Link>
+
+        <section className={s.infoSection}>
+          <ul className={s.nav}>
+            <li>
+              <Link to="/technology">Technology</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/careers">Careers</Link>
+            </li>
+          </ul>
+
+          <div className={s.details}>
+            <span>Tel: +‭1 (949) 682-9479‬</span>
+            <a href="mailto:info@asterinformation.com">
+              info@asterinformation.com
+            </a>
+            <p>
+              Suite 4, 2nd Floor, The West Wing, Montarik House, 3 Bedlam Court,
+              GX1 1 1AA, Gibraltar
+            </p>
+          </div>
+        </section>
+
+        <section className={s.subscribeSection}>
+          <h2>SUBSCRIBE</h2>
+          <p>Sign up to receive Autono news and updates.</p>
+
+          <form>
+            <label htmlFor="sub-email">Email *</label>
+            <div className={s.input}>
+              <input type="email" name="sub-email" id="sub-email" required />
+              <button type="submit">Subscribe</button>
+            </div>
+          </form>
+        </section>
+      </div>
+
+      <section className={s.footerBottom}>
+        <p className={s.copyright}>© 2023 by Aster Information Limited</p>
+
+        <ul className={s.socialMedia}>
+          <li>
+            <a href="#">
+              <img src={linkedinLogo} alt="LinkedIn logo" />
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src={facebookLogo} alt="Facebook logo" />
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src={twitterLogo} alt="Twitter logo" />
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src={instagramLogo} alt="Instagram logo" />
+            </a>
+          </li>
+        </ul>
+      </section>
+    </footer>
+  );
 };
 export default Footer;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
       </div>
 
       <VisionSection />
-      <ServicesSection />
+      {/* <ServicesSection /> */}
     </main>
   );
 };
