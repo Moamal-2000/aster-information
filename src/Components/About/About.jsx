8e895210c4c8afb294fee836d460e9cdda465754
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-19 08:32:18 +0300

    feat: initiate About page component

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,18 @@
+import { Helmet } from "react-helmet-async";
+import s from "./About.module.scss";
+
+const About = () => {
+  return (
+    <>
+      <Helmet>
+        <title>About | Aster Information</title>
+        <meta name="description" content="About page description" />
+      </Helmet>
+
+      <main className={s.about} id="about-page">
+        About
+      </main>
+    </>
+  );
+};
+export default About;
