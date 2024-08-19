a1f9e0f7af2ef0f8be30a6541b63750ab16560a8
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-19 08:30:39 +0300

    feat: initiate Technology page component

---
+++ src/Components/Technology/Technology.jsx
@@ -0,0 +1,18 @@
+import { Helmet } from "react-helmet-async";
+import s from "./Technology.module.scss";
+
+const Technology = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Technology | Aster Information</title>
+        <meta name="description" content="Technology page description" />
+      </Helmet>
+
+      <main className={s.technology} id="technology-page">
+        Technology
+      </main>
+    </>
+  );
+};
+export default Technology;
