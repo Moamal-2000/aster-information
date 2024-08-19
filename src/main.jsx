3570f2d8a079fc85aac56ea2c1bde8dfc3a95463
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-19 08:23:47 +0300

    fix: wrap App with HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./App/store.js";
@@ -8,7 +9,9 @@ import "./Styles/index.scss";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </Provider>
   </StrictMode>
 );
