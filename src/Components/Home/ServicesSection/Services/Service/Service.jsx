267dfa07bc6fa00baa5d51ec374739789ffb4a4d
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-20 12:52:26 +0300

    feat: basic Service section styles

--- src/Components/Home/ServicesSection/Services/Service/Service.jsx
+++ src/Components/Home/ServicesSection/Services/Service/Service.jsx
@@ -1,8 +1,10 @@
-import s from "./Service.module.scss"
+import s from "./Service.module.scss";
 
-const Service = ({dir, children}) => {
+const Service = ({ dir, children }) => {
   return (
-    <div dir={dir}>{children}</div>
-  )
-}
-export default Service
+    <div className={s.service} dir={dir}>
+      {children}
+    </div>
+  );
+};
+export default Service;
--- src/Components/Home/ServicesSection/Services/Services.jsx
+++ src/Components/Home/ServicesSection/Services/Services.jsx
@@ -1,4 +1,8 @@
-import { serviceSectionBg1 } from "../../../../Assets/Images/Images";
+import {
+  serviceSectionBg1,
+  serviceSectionBg2,
+  serviceSectionBg3,
+} from "src/Assets/Images/Images";
 import Service from "./Service/Service";
 import ServiceContent from "./Service/ServiceContent";
 import s from "./Services.module.scss";
@@ -7,19 +11,62 @@ const Services = () => {
   return (
     <div className={s.services}>
       <Service dir="ltr">
-        <ServiceContent
-          title={"INTEGRATED MANAGEMENT"}
-          description={`Comprehensive Information and Data Management
+        <div className="container">
+          <ServiceContent
+            title={"INTEGRATED MANAGEMENT"}
+            description={`Comprehensive Information and Data Management
             services form the core of our offerings. We aim to organize,
             secure, and streamline your business data,
             maximizing its value. Our services encompass database
             management, data quality, security, and information strategy,
             driving value and business outcomes.`}
-          buttonText={"Read More"}
-          buttonPath={"/technology"}
-        />
+            buttonText={"Read More"}
+            buttonPath={"/technology"}
+          />
+        </div>
 
-        <img src={serviceSectionBg1} alt="" />
+        <div className={s.serviceImgHolder}>
+          <img src={serviceSectionBg1} alt="" />
+        </div>
+      </Service>
+
+      <Service dir="rtl">
+        <div className="container">
+          <ServiceContent
+            title={"SOFTWARE SOLUTIONS"}
+            description={`We offer custom software development services designed
+              to meet your business needs. Our team specializes in creating
+              efficient, scalable, and robust software solutions that can
+              transform your business operations. From crafting intuitive mobile
+              apps to architecting complex enterprise systems, we deliver
+              solutions tailored to your needs.`}
+            buttonText={"Read More"}
+            buttonPath={"/technology"}
+          />
+        </div>
+
+        <div className={s.serviceImgHolder}>
+          <img src={serviceSectionBg2} alt="" />
+        </div>
+      </Service>
+
+      <Service dir="ltr">
+        <div className="container">
+          <ServiceContent
+            title={"IT CONSULTING"}
+            description={`Our IT Consulting services provide strategic guidance in
+              leveraging technology for growth. We offer system design and project
+              management, ensuring your technology infrastructure aligns with your
+              business objectives. Trust us to be your navigators in the complex
+              and ever-evolving tech landscape.`}
+            buttonText={"Read More"}
+            buttonPath={"/technology"}
+          />
+        </div>
+
+        <div className={s.serviceImgHolder}>
+          <img src={serviceSectionBg3} alt="" />
+        </div>
       </Service>
     </div>
   );
