e9379b88863e89845115f10a335472c2e257d1a8
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-22 21:30:49 +0300

    feat: basic styles for WhyUsSection card

--- src/Components/Home/WhyUs/WhyUsSection.jsx
+++ src/Components/Home/WhyUs/WhyUsSection.jsx
@@ -1,4 +1,5 @@
 import { WEBSITE_NAME } from "src/Data/variables";
+import AnimatedLine from "../../Shared/MiniComponents/AnimatedLine/AnimatedLine";
 import s from "./WhyUsSection.module.scss";
 
 const WhyUsSection = () => {
@@ -7,20 +8,37 @@ const WhyUsSection = () => {
       <div className={s.backgroundImg} />
 
       <div className="container">
-        <h2 className={s.title}>Why {WEBSITE_NAME}</h2>
+        <div className={s.card}>
+          <div className={s.animatedLine}>
+            <AnimatedLine
+              lineHeight="631px"
+              yAxis="129px"
+              animateTo="70px"
+              lineColors={{
+                lineColor: "#b4b4b4",
+                animatedLineColor: "#fff",
+              }}
+            />
+          </div>
 
-        <p className={s.subTitle}>
-          A different approach, using a new method of Information Management.
-        </p>
+          <div className={s.content}>
+            <h2 className={s.title}>Why {WEBSITE_NAME}</h2>
 
-        <p className={s.description}>
-          Experience a distinct edge in Information Management and IT solutions.
-          With advanced techniques and cutting-edge technology, we deliver
-          tailored solutions that drive operational efficiency and strategic
-          growth across industries.
-        </p>
+            <p className={s.subTitle}>
+              A different approach, using a new method of Information
+              Management.
+            </p>
 
-        <button type="button">Read More</button>
+            <p className={s.description}>
+              Experience a distinct edge in Information Management and IT
+              solutions. With advanced techniques and cutting-edge technology,
+              we deliver tailored solutions that drive operational efficiency
+              and strategic growth across industries.
+            </p>
+
+            <button type="button">Read More</button>
+          </div>
+        </div>
       </div>
     </section>
   );
