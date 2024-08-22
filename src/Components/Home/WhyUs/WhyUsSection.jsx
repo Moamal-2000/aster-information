b64add52998e88c3ac251125485519d4cd53fcff
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-22 21:17:50 +0300

    feat: build WhyUsSection's stricture

--- src/Components/Home/WhyUs/WhyUsSection.jsx
+++ src/Components/Home/WhyUs/WhyUsSection.jsx
@@ -1,11 +1,26 @@
+import { WEBSITE_NAME } from "src/Data/variables";
 import s from "./WhyUsSection.module.scss";
 
 const WhyUsSection = () => {
   return (
     <section className={s.whyUsSection}>
-        <div className={s.backgroundImg} />
+      <div className={s.backgroundImg} />
 
       <div className="container">
+        <h2 className={s.title}>Why {WEBSITE_NAME}</h2>
+
+        <p className={s.subTitle}>
+          A different approach, using a new method of Information Management.
+        </p>
+
+        <p className={s.description}>
+          Experience a distinct edge in Information Management and IT solutions.
+          With advanced techniques and cutting-edge technology, we deliver
+          tailored solutions that drive operational efficiency and strategic
+          growth across industries.
+        </p>
+
+        <button type="button">Read More</button>
       </div>
     </section>
   );
