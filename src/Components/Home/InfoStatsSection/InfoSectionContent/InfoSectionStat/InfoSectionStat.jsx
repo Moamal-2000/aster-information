4b883953c07b0aefd3146cbcb75cee6138eb67b9
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-23 19:53:41 +0300

    fix: import HorizontalAnimatedLine component

--- src/Components/Home/InfoStatsSection/InfoSectionContent/InfoSectionStat/InfoSectionStat.jsx
+++ src/Components/Home/InfoStatsSection/InfoSectionContent/InfoSectionStat/InfoSectionStat.jsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import useHasAnimatedOnScroll from "src/Hooks/App/useHasAnimatedOnScroll";
 import useOnScreen from "src/Hooks/Helper/useOnScreen";
+import HorizontalAnimatedLine from "../../../../Shared/MiniComponents/horizontalAnimatedLine/horizontalAnimatedLine";
 import s from "./InfoSectionStat.module.scss";
 
 const InfoSectionStat = ({ stat: { title, value, lineDelay } }) => {
