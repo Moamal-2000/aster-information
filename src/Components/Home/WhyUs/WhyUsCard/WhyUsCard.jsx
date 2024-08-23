c14084f9762356a26b1a85794e5ad78a058c144c
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-23 14:08:06 +0300

    feat: separate WhyUsSection card into WhyUsCard component

---
+++ src/Components/Home/WhyUs/WhyUsCard/WhyUsCard.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+import useHasAnimatedOnScroll from "src/Hooks/App/useHasAnimatedOnScroll";
+import useOnScreen from "src/Hooks/Helper/useOnScreen";
+import AnimatedLine from "../../../Shared/MiniComponents/AnimatedLine/AnimatedLine";
+import WhyUsContent from "../WhyUsContent/WhyUsContent";
+import s from "./WhyUsCard.module.scss";
+
+const WhyUsCard = () => {
+  const cardRef = useRef();
+  const [rootMargin, setRootMargin] = useState("0px");
+  const isElementVisible = useOnScreen(cardRef, { rootMargin, threshold: 1 });
+  const hasAnimated = useHasAnimatedOnScroll(isElementVisible);
+  const activeClass = hasAnimated ? s.active : "";
+
+  useEffect(() => {
+    setRootMargin(cardRef?.current?.scrollHeight + "px");
+  }, []);
+
+  return (
+    <div className={`${s.card} ${activeClass}`} ref={cardRef}>
+      <div className={s.animatedLine}>
+        <AnimatedLine
+          lineHeight="505px"
+          yAxis="150px"
+          animateTo="60px"
+          lineColors={{
+            lineColor: "#b4b4b4",
+            animatedLineColor: "#fff",
+          }}
+        />
+      </div>
+
+      <WhyUsContent />
+    </div>
+  );
+};
+export default WhyUsCard;
--- src/Components/Home/WhyUs/WhyUsSection.jsx
+++ src/Components/Home/WhyUs/WhyUsSection.jsx
@@ -1,41 +1,13 @@
-import { useEffect, useRef, useState } from "react";
-import useHasAnimatedOnScroll from "src/Hooks/App/useHasAnimatedOnScroll";
-import useOnScreen from "src/Hooks/Helper/useOnScreen";
-import AnimatedLine from "../../Shared/MiniComponents/AnimatedLine/AnimatedLine";
-import WhyUsContent from "./WhyUsContent/WhyUsContent";
+import WhyUsCard from "./WhyUsCard/WhyUsCard";
 import s from "./WhyUsSection.module.scss";
 
 const WhyUsSection = () => {
-  const cardRef = useRef();
-  const [rootMargin, setRootMargin] = useState("0px");
-  const isElementVisible = useOnScreen(cardRef, { rootMargin, threshold: 1 });
-  const hasAnimated = useHasAnimatedOnScroll(isElementVisible);
-  const activeClass = hasAnimated ? s.active : "";
-
-  useEffect(() => {
-    setRootMargin(cardRef?.current?.scrollHeight + "px");
-  }, []);
-
   return (
     <section className={s.whyUsSection}>
       <div className={s.backgroundImg} />
 
       <div className="container">
-        <div className={`${s.card} ${activeClass}`} ref={cardRef}>
-          <div className={s.animatedLine}>
-            <AnimatedLine
-              lineHeight="505px"
-              yAxis="150px"
-              animateTo="60px"
-              lineColors={{
-                lineColor: "#b4b4b4",
-                animatedLineColor: "#fff",
-              }}
-            />
-          </div>
-
-          <WhyUsContent />
-        </div>
+        <WhyUsCard />
       </div>
     </section>
   );
