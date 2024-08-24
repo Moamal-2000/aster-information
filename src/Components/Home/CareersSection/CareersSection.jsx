4b52a8a331145d6295517f0e97ca035ab8673e10
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-25 01:21:21 +0300

    feat: stop auto switch functionality on 'mouseenter' event for CareersSlider

--- src/Components/Home/CareersSection/CareersSection.jsx
+++ src/Components/Home/CareersSection/CareersSection.jsx
@@ -7,7 +7,6 @@ const CareersSection = () => {
     <section className={s.careersSection}>
       <div className="container" data-container>
         <CareersSectionContent />
-
         <CareersSlider />
       </div>
     </section>
--- src/Components/Home/CareersSection/CareersSlider/CareersSlider.jsx
+++ src/Components/Home/CareersSection/CareersSlider/CareersSlider.jsx
@@ -1,25 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { careersSliderData } from "src/Data/staticData";
 import { CAREERS_SLIDER_SWITCH_TIME } from "src/Data/variables";
+import useEventListener from "src/Hooks/Helper/useEventListener";
 import CareerSlide from "./CareerSlide/CareerSlide";
 import CareersPaginationDots from "./CareersPaginationDots/CareersPaginationDots";
 import s from "./CareersSlider.module.scss";
 
 const CareersSlider = () => {
   const [activeSlide, setActiveSlide] = useState(1);
+  const sliderRef = useRef();
+  let timerId;
 
-  useEffect(() => {
-    const timerId = setTimeout(() => {
+  useEventListener(sliderRef, "mouseenter", () => {
+    clearTimeout(timerId);
+  });
+
+  useEventListener(sliderRef, "mouseleave", handleSlideSwitching);
+
+  function handleSlideSwitching() {
+    timerId = setTimeout(() => {
       setActiveSlide((currentSlide) =>
-        activeSlide < careersSliderData.length ? currentSlide + 1 : 1
+        activeSlide < careersSliderData.length ? ++currentSlide : 1
       );
     }, CAREERS_SLIDER_SWITCH_TIME);
+  }
+
+  useEffect(() => {
+    handleSlideSwitching();
 
     return () => clearTimeout(timerId);
   }, [activeSlide]);
 
   return (
-    <div className={s.slider}>
+    <div className={s.slider} ref={sliderRef}>
       <div className={s.slideSpace} />
 
       {careersSliderData.map(({ id, title, location, description }) => (
