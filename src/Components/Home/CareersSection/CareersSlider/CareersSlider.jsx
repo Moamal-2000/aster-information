dc7ef66e8c5ff52b857afe76930e1dacc1765e59
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-24 23:36:20 +0300

    feat: build the stricture of CareersSlider

--- src/Components/Home/CareersSection/CareersSlider/CareersSlider.jsx
+++ src/Components/Home/CareersSection/CareersSlider/CareersSlider.jsx
@@ -1,6 +1,31 @@
+import Button from "../../../Shared/Buttons/Button/Button";
 import s from "./CareersSlider.module.scss";
 
 const CareersSlider = () => {
-  return <div className={s.slider}></div>;
+  return (
+    <div className={s.slider}>
+      <div className={s.slide}>
+        <h2 className={s.title}>INTEGRATION ENGINEER</h2>
+
+        <p className={s.location}>
+          <strong>Remote</strong>
+        </p>
+
+        <p className={s.description}>
+          Seeking a detail-oriented Integration Engineer skilled in system
+          architecture, proficient in multiple languages, and passionate about
+          data-driven solutions.
+        </p>
+
+        <Button
+          text="Apply Now"
+          path="/electrical-engineer"
+          invertColors={true}
+        />
+      </div>
+
+      {/* <CareersPaginationDots */}
+    </div>
+  );
 };
 export default CareersSlider;
