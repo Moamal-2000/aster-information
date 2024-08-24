06189e62945e0add882f6def14f1d07cee7815e3
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-25 00:36:12 +0300

    feat: show slide by its index for CareersSlider

--- src/Components/Home/CareersSection/CareersSlider/CareerSlide/CareerSlide.jsx
+++ src/Components/Home/CareersSection/CareersSlider/CareerSlide/CareerSlide.jsx
@@ -1,9 +1,11 @@
 import Button from "../../../../Shared/Buttons/Button/Button";
 import s from "./CareerSlide.module.scss";
 
-const CareerSlide = ({ title, location, description }) => {
+const CareerSlide = ({ title, location, description, activeSlider }) => {
+  const activeClass = activeSlider ? s.active : "";
+
   return (
-    <div className={s.slide}>
+    <div className={`${s.slide} ${activeClass}`}>
       <h2 className={s.title}>{title}</h2>
 
       <p className={s.location}>
--- src/Components/Home/CareersSection/CareersSlider/CareersSlider.jsx
+++ src/Components/Home/CareersSection/CareersSlider/CareersSlider.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { careersSliderData } from "src/Data/staticData";
 import CareerSlide from "./CareerSlide/CareerSlide";
 import s from "./CareersSlider.module.scss";
 
 const CareersSlider = () => {
+  const [activeSlide, setActiveSlide] = useState(1);
+
   return (
     <div className={s.slider}>
       {careersSliderData.map(({ id, title, location, description }) => (
@@ -11,8 +14,11 @@ const CareersSlider = () => {
           title={title}
           location={location}
           description={description}
+          activeSlider={id === activeSlide}
         />
       ))}
+
+      <div className={s.slideSpace} />
 
       {/* <CareersPaginationDots */}
     </div>
