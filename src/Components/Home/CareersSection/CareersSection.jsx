1b4758eedd0e152e578dc5ac7c6e15fd6562dac6
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-24 23:24:25 +0300

    feat: initiate CareersSlider

--- src/Components/Home/CareersSection/CareersSection.jsx
+++ src/Components/Home/CareersSection/CareersSection.jsx
@@ -1,11 +1,14 @@
 import s from "./CareersSection.module.scss";
 import CareersSectionContent from "./CareersSectionContent/CareersSectionContent";
+import CareersSlider from "./CareersSlider/CareersSlider";
 
 const CareersSection = () => {
   return (
     <section className={s.careersSection}>
       <div className="container" data-container>
         <CareersSectionContent />
+
+        <CareersSlider />
       </div>
     </section>
   );
---
+++ src/Components/Home/CareersSection/CareersSlider/CareersSlider.jsx
@@ -0,0 +1,6 @@
+import s from "./CareersSlider.module.scss";
+
+const CareersSlider = () => {
+  return <div className={s.slider}></div>;
+};
+export default CareersSlider;
