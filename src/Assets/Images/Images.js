2693420096ae1773e174d8c8ff084c7aee88200b
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-25 21:47:53 +0300

    perf: improve performance, compress home hero section image & convert all images to webp

--- src/Assets/Images/Images.js
+++ src/Assets/Images/Images.js
@@ -4,8 +4,8 @@ import dataManagementBg from "./data-management-section-bg.webp";
 import docusignLogo from "./docusign-logo.webp";
 import facebookLogo from "./facebook.webp";
 import generalTransportLogo from "./general-transport.webp";
-import homeBgBottom from "./home-bg-bottom.png";
-import homeBgTop from "./home-bg-top.png";
+import homeBgBottom from "./home-bg-bottom.webp";
+import homeBgTop from "./home-bg-top.webp";
 import idiSoftwareLogo from "./IDI-software.webp";
 import imogenCarsLogo from "./imogen-cars.webp";
 import infoSectionBg from "./info-section-bg.webp";
@@ -14,18 +14,18 @@ import joinOurTeamBg from "./join-our-team-bg.webp";
 import linkedinLogo from "./linkedin.webp";
 import locationSectionBg from "./location-section-bg.webp";
 import microsoftLogo from "./microsoft-logo.webp";
-import ourMissionSectionBg from "./our-mission-section-bg.jpg";
+import ourMissionSectionBg from "./our-mission-section-bg.webp";
 import paloAltotRibunal from "./palo-altot-ribunal.webp";
 import pngaaaLogo from "./pngaaa-logo.webp";
 import serviceSectionBg1 from "./services-bg1.webp";
 import serviceSectionBg2 from "./services-bg2.webp";
 import serviceSectionBg3 from "./services-bg3.webp";
-import teamCollaborationImg from "./team-collaboration.jpg";
+import teamCollaborationImg from "./team-collaboration.webp";
 import techPageBg from "./tech-bg.webp";
 import triNexLogo from "./tri-nex .webp";
 import twitterLogo from "./twitter.webp";
-import visionSectionBg from "./vision-section-bg.png";
-import whyUsSectionBg from "./why-us-section-bg.jpg";
+import visionSectionBg from "./vision-section-bg.webp";
+import whyUsSectionBg from "./why-us-section-bg.webp";
 
 export {
   amazonAwsLogo,
