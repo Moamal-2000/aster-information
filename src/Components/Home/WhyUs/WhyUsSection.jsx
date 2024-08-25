eeae7e90bf2ee4aebed318b8e9ef616be002bf74
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-25 22:43:11 +0300

    perf: improve accessibility, use img element instead of div for WhyUsSection background

--- src/Components/Home/WhyUs/WhyUsSection.jsx
+++ src/Components/Home/WhyUs/WhyUsSection.jsx
@@ -1,10 +1,16 @@
+import { whyUsSectionBg } from "src/Assets/Images/Images";
 import WhyUsCard from "./WhyUsCard/WhyUsCard";
 import s from "./WhyUsSection.module.scss";
 
 const WhyUsSection = () => {
   return (
     <section className={s.whyUsSection}>
-      <div className={s.backgroundImg} />
+      <img
+        src={whyUsSectionBg}
+        alt="Why-us section background image"
+        loading="lazy"
+        className={s.backgroundImg}
+      />
 
       <div className="container">
         <WhyUsCard />
