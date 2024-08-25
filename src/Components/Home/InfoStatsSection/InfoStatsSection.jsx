aed4073b511c1b428514fdffaef585d992eb6f20
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-25 22:45:58 +0300

    perf: improve accessibility #2, use img element instead of div for InfoStatsSection

--- src/Components/Home/InfoStatsSection/InfoStatsSection.jsx
+++ src/Components/Home/InfoStatsSection/InfoStatsSection.jsx
@@ -1,10 +1,16 @@
+import { infoSectionBg } from "src/Assets/Images/Images";
 import InfoSectionContent from "./InfoSectionContent/InfoSectionContent";
 import s from "./InfoStatsSection.module.scss";
 
 const InfoStatsSection = () => {
   return (
     <section className={s.infoSection}>
-      <div className={s.backgroundImg} />
+      <img
+        src={infoSectionBg}
+        alt="Info stats section image"
+        loading="lazy"
+        className={s.backgroundImg}
+      />
 
       <div className="container" data-container>
         <InfoSectionContent />
