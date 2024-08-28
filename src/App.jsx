7a2bf20a8b85f1fe41dcfada0af01d8720c8c281
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-28 09:50:46 +0300

    test: prevent user from over-scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import useEventListener from "./Hooks/Helper/useEventListener";
 import AppRoutes from "./Routes/AppRoutes";
 
 function App() {
+  useEventListener(document.body, "touchmove", (event) => {
+    if (event.source == document.body) event.preventDefault();
+  });
+
   return <AppRoutes />;
 }
 
