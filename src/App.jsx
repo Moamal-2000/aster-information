1621b5df854ca70440003ca889f9b012a1a569d0
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-28 10:31:35 +0300

    fix: hide why-us section background image if user scrolled far of the section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import useEventListener from "./Hooks/Helper/useEventListener";
 import AppRoutes from "./Routes/AppRoutes";
 
 function App() {
-  useEventListener(document.body, "touchmove", (event) => {
-    if (event.source == document.body) event.preventDefault();
-  });
-
   return <AppRoutes />;
 }
 
--- src/Components/Home/WhyUs/WhyUsSection.jsx
+++ src/Components/Home/WhyUs/WhyUsSection.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import { whyUsSectionBg } from "src/Assets/Images/Images";
+import useEventListener from "../../../Hooks/Helper/useEventListener";
 import WhyUsCard from "./WhyUsCard/WhyUsCard";
 import s from "./WhyUsSection.module.scss";
 
 const WhyUsSection = () => {
+  const [shouldShowImg, setShouldShowImg] = useState(false);
+  const activeClass = shouldShowImg ? s.active : "";
+
+  useEventListener(
+    window,
+    "scroll",
+    () => {
+      const isAtVeryBottom =
+        window.scrollY + window.innerHeight + 100 >= pageHeight;
+      const pageHeight = document.documentElement.offsetHeight;
+
+      setShouldShowImg(!(isAtVeryBottom || window.scrollY <= 0));
+    },
+    []
+  );
+
   return (
-    <section className={s.whyUsSection}>
+    <section className={`${s.whyUsSection} ${activeClass}`}>
       <img
         src={whyUsSectionBg}
         alt="Why-us section background image"
