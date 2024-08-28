6f0b2f4c9ac06fd9222fe70518b37bd18bc4e5de
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-28 03:38:51 +0300

    feat: change the appearance of the header on scroll state

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,30 @@
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { WEBSITE_NAME } from "src/Data/variables";
+import { SCROLL_TRIGGER_POINT, WEBSITE_NAME } from "src/Data/variables";
+import useEventListener from "src/Hooks/Helper/useEventListener";
 import SvgIcon from "../Shared/MiniComponents/SvgIcon";
 import s from "./Header.module.scss";
 import MobileNav from "./MobileNav/MobileNav";
 import NavIcon from "./NavIcon/NavIcon";
 
 const Header = () => {
+  const headerRef = useRef();
+  const scrollDebounceTimer = useRef();
+  const [activeClass, setActiveClass] = useState("");
+
+  useEventListener(window, "wheel", (event) => {
+    clearTimeout(scrollDebounceTimer.current);
+
+    scrollDebounceTimer.current = setTimeout(
+      () => updateHeaderActiveClass(event, setActiveClass),
+      100
+    );
+  });
+
   return (
-    <header className={s.header}>
+    <header className={`${s.header} ${activeClass}`} ref={headerRef}>
       <div className="container">
-        <Link to="/">
+        <Link to="/" aria-label="Home page">
           <SvgIcon name="mainIcon" />
           <span>{WEBSITE_NAME}</span>
         </Link>
@@ -21,3 +36,10 @@ const Header = () => {
   );
 };
 export default Header;
+
+function updateHeaderActiveClass(event, setActiveClass) {
+  const hasScrolledBeyondTrigger = window.scrollY >= SCROLL_TRIGGER_POINT;
+  const isScrollingDown = event.deltaY >= 0;
+
+  setActiveClass(isScrollingDown && hasScrolledBeyondTrigger ? s.active : "");
+}
