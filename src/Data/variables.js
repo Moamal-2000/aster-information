3740c443766d8f8d06f0049c6ad6bbb982f43236
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-08-30 04:49:44 +0300

    fix: correct misspelling of website name

--- src/Data/variables.js
+++ src/Data/variables.js
@@ -2,7 +2,7 @@ export const HIDE_SCROLL_TOP_MS = 4000;
 export const CAREERS_SLIDER_SWITCH_TIME = 5000;
 export const SUBMIT_EMAIL_MESSAGE_MS = 4000;
 export const SCROLL_TRIGGER_POINT = 480;
-export const WEBSITE_NAME = "Ister Information";
+export const WEBSITE_NAME = "Aster Information";
 export const SCREEN_SIZES = {
   desktop: 1200,
   laptop: 992,
