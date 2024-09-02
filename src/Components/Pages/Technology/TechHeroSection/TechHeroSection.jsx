bb8312e2277a29dbdc7dc59ac152dc84f883110c
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-02 22:22:15 +0300

    style: absolute responsive for TechHeroSection

--- src/Components/Pages/Technology/TechHeroSection/TechHeroSection.jsx
+++ src/Components/Pages/Technology/TechHeroSection/TechHeroSection.jsx
@@ -4,7 +4,11 @@ import s from "./TechHeroSection.module.scss";
 const TechHeroSection = () => {
   return (
     <section className={s.technology}>
-      <img className={s.technologyBg} src={techPageBg} alt="Technology Page Background" />
+      <img
+        className={s.technologyBg}
+        src={techPageBg}
+        alt="Technology Page Background"
+      />
       <h1>INNOVATIVE DATA-DRIVEN SOLUTIONS</h1>
     </section>
   );
