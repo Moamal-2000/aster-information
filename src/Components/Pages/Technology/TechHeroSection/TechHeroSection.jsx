0cd6faf8b3aca55bcca204abdf20b9d1317bd5a4
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-02 22:13:23 +0300

    style: done of styling TechHeroSection

--- src/Components/Pages/Technology/TechHeroSection/TechHeroSection.jsx
+++ src/Components/Pages/Technology/TechHeroSection/TechHeroSection.jsx
@@ -1,10 +1,10 @@
-import { techPageBg } from "../../../../Assets/Images/Images";
+import { techPageBg } from "src/Assets/Images/Images";
 import s from "./TechHeroSection.module.scss";
 
 const TechHeroSection = () => {
   return (
     <section className={s.technology}>
-      <img src={techPageBg} alt="Technology Page Background" />
+      <img className={s.technologyBg} src={techPageBg} alt="Technology Page Background" />
       <h1>INNOVATIVE DATA-DRIVEN SOLUTIONS</h1>
     </section>
   );
