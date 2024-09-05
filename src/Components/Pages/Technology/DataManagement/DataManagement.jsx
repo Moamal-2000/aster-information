5598130f73abc39ef6b72ebdcf8a26aae4f8957f
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-05 10:27:38 +0300

    feat: initiate DataManagement

---
+++ src/Components/Pages/Technology/DataManagement/DataManagement.jsx
@@ -0,0 +1,6 @@
+import s from "./DataManagement.module.scss";
+
+const DataManagement = () => {
+  return <section className={s.dataManagement}>DataManagement</section>;
+};
+export default DataManagement;
