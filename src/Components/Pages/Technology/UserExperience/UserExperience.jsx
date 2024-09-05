6bcbf74c4723f544bfd99c0ec08cb0011ee0356b
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-05 09:59:17 +0300

    feat: initiate UserExperienceStack

--- src/Components/Pages/Technology/UserExperience/UserExperience.jsx
+++ src/Components/Pages/Technology/UserExperience/UserExperience.jsx
@@ -1,9 +1,17 @@
 import s from "./UserExperience.module.scss";
+import UserExperienceStack from "./UserExperienceStack/UserExperienceStack";
 
 const UserExperience = () => {
   return (
     <section className={s.userExperience}>
-      <div className="container" data-container></div>
+      <div className="container" data-container>
+        <UserExperienceStack title="ENHANCED USER EXPERIENCE">
+          our focus is on creating user-friendly, intuitive software solutions
+          that enhance productivity and streamline operations. We are committed
+          to improving the user experience by designing systems that simplify
+          complex tasks, making them more manageable and efficient.
+        </UserExperienceStack>
+      </div>
     </section>
   );
 };
---
+++ src/Components/Pages/Technology/UserExperience/UserExperienceStack/UserExperienceStack.jsx
@@ -0,0 +1,11 @@
+import s from "./UserExperienceStack.module.scss";
+
+const UserExperienceStack = ({title, children}) => {
+  return (
+    <div className={s.stackContent}>
+      <h2>{title}</h2>
+      <p>{children}</p>
+    </div>
+  )
+}
+export default UserExperienceStack
