0de3f06e20fe0abd2f6b8fbe1a5d78d20c27dcf1
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-05 11:49:32 +0300

    feat: animate UserExperienceStack on scroll

--- src/Components/Pages/Technology/UserExperience/UserExperienceStack/UserExperienceStack.jsx
+++ src/Components/Pages/Technology/UserExperience/UserExperienceStack/UserExperienceStack.jsx
@@ -1,10 +1,18 @@
+import { useRef } from "react";
+import useClassOnFirstView from "src/Hooks/App/useClassOnFirstView";
 import s from "./UserExperienceStack.module.scss";
 
 const UserExperienceStack = ({ title, children }) => {
+  const stackRef = useRef();
+  const activeClass = useClassOnFirstView({
+    elementRef: stackRef,
+    cssModule: s,
+  });
+
   return (
-    <div className={s.stackContent}>
-      <h2>{title}</h2>
-      <p>{children}</p>
+    <div className={s.stackContent} ref={stackRef}>
+      <h2 className={activeClass}>{title}</h2>
+      <p className={activeClass}>{children}</p>
     </div>
   );
 };
