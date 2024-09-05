b365557eb5d412d9d046d29b28e5637fcaedefc7
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-05 10:09:56 +0300

    style: UserExperience section

--- src/Components/Pages/Technology/UserExperience/UserExperienceStack/UserExperienceStack.jsx
+++ src/Components/Pages/Technology/UserExperience/UserExperienceStack/UserExperienceStack.jsx
@@ -1,11 +1,11 @@
 import s from "./UserExperienceStack.module.scss";
 
-const UserExperienceStack = ({title, children}) => {
+const UserExperienceStack = ({ title, children }) => {
   return (
     <div className={s.stackContent}>
       <h2>{title}</h2>
       <p>{children}</p>
     </div>
-  )
-}
-export default UserExperienceStack
+  );
+};
+export default UserExperienceStack;
