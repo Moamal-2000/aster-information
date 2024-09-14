bbca6b2913b67997a1e2979ec5c5c61fa63fb7e4
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-15 01:24:15 +0300

    feat: build the structure of AboutHeroSection & basic styling

--- src/Components/Pages/About/AboutHeroSection/AboutHeroSection.jsx
+++ src/Components/Pages/About/AboutHeroSection/AboutHeroSection.jsx
@@ -1,6 +1,19 @@
 import s from "./AboutHeroSection.module.scss";
 
 const AboutHeroSection = () => {
-  return <section className={s.aboutHeroSection}>AboutHeroSection</section>;
+  return (
+    <section className={s.aboutHeroSection}>
+      <h1>OUR JOURNEY</h1>
+      <p>
+        As a startup, our journey at Aster Information Limited has been defined
+        by audacious innovation and resilience. From our inception, we've been
+        committed to transforming the realms of Information Management, Data
+        Management, IT Consultation, and Software Development. Our story is one
+        of continuous growth and learning, as we navigate the challenges and
+        opportunities of this dynamic industry with a steadfast commitment to
+        excellence and progress.
+      </p>
+    </section>
+  );
 };
 export default AboutHeroSection;
