214976084df4b318389e37b6cf0fbb38ff98fd59
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-22 20:53:46 +0300

    feat: animate partners logos on scroll

--- src/Components/Pages/About/PartnersSection/Partners/Partners.jsx
+++ src/Components/Pages/About/PartnersSection/Partners/Partners.jsx
@@ -1,9 +1,17 @@
+import { useRef } from "react";
 import { partnersSectionData } from "src/Data/staticData";
+import useClassOnFirstView from "src/Hooks/App/useClassOnFirstView";
 import s from "./Partners.module.scss";
 
 const Partners = () => {
+  const partnerRef = useRef();
+  const activeClass = useClassOnFirstView({
+    elementRef: partnerRef,
+    cssModule: s,
+  });
+
   return (
-    <div className={s.partners}>
+    <div className={`${s.partners} ${activeClass}`} ref={partnerRef}>
       {partnersSectionData.map(({ id, img, partner }) => (
         <img key={id} src={img} alt={partner} loading="lazy" />
       ))}
