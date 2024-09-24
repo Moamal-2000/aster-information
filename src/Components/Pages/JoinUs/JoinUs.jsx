f2f5d126891395fd7d5636069a82484c66f29456
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-24 22:41:57 +0300

    feat: improve SEO for JoinUs page through Helmet library

--- src/Components/Pages/JoinUs/JoinUs.jsx
+++ src/Components/Pages/JoinUs/JoinUs.jsx
@@ -1,10 +1,19 @@
+import { Helmet } from "react-helmet-async";
+import { WEBSITE_NAME } from "src/Data/variables";
 import s from "./JoinUs.module.scss";
 
 const JoinUs = () => {
   return (
-    <main className={s.joinUsPage} id="joinUsPage">
-      <h1>Join Us</h1>
-    </main>
+    <>
+      <Helmet>
+        <title>Join Us | {WEBSITE_NAME}</title>
+        <meta name="description" content="Join Us page description" />
+      </Helmet>
+
+      <main className={s.joinUsPage} id="joinUsPage">
+        <h1>Join Us</h1>
+      </main>
+    </>
   );
 };
 export default JoinUs;
