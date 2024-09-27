4f7de3fb2b73aa238319fc9bd10737241f79b1c3
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-27 12:10:26 +0300

    perf: improve accessibility for the About page

--- src/Components/Pages/About/JourneySection/JourneySlider/JourneySliderButtons/JourneySliderButtons.jsx
+++ src/Components/Pages/About/JourneySection/JourneySlider/JourneySliderButtons/JourneySliderButtons.jsx
@@ -7,14 +7,16 @@ const JourneySliderButtons = ({
   activeClassProp,
 }) => {
   const activeClass = activeClassProp ? s.active : "";
+  const isFocusable = activeClassProp ? "0" : "-1";
 
   return (
-    <div className={s.sliderButtons}>
+    <div className={`${s.sliderButtons} ${activeClass}`}>
       <button
         type="button"
         className={s.prevButton}
         onClick={handlePrevSlide}
         aria-label="Previous timeline"
+        tabIndex={isFocusable}
       >
         <SvgIcon name="chevronLeft" />
       </button>
@@ -26,6 +28,7 @@ const JourneySliderButtons = ({
         className={s.nextButton}
         onClick={handleNextSlide}
         aria-label="Next timeline"
+        tabIndex={isFocusable}
       >
         <SvgIcon name="chevronLeft" />
       </button>
