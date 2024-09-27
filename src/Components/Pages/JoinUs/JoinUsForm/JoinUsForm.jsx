135b7dac1aa49abc2a35570df5054fd10da56d52
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-27 10:41:52 +0300

    style: responsive JoinUsForm and Input components until 320px screen sizes

--- src/Components/Pages/JoinUs/JoinUsForm/JoinUsForm.jsx
+++ src/Components/Pages/JoinUs/JoinUsForm/JoinUsForm.jsx
@@ -4,11 +4,13 @@ import JoinUsInputs from "./JoinUsInputs/JoinUsInputs";
 const JoinUsForm = () => {
   return (
     <div className={s.joinUsWrapper}>
-      <form className={s.joinUsForm}>
-        <h2>Submit Your Application</h2>
+      <div className="container">
+        <form className={s.joinUsForm}>
+          <h2>Submit Your Application</h2>
 
-        <JoinUsInputs />
-      </form>
+          <JoinUsInputs />
+        </form>
+      </div>
     </div>
   );
 };
