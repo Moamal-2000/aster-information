e7b11c9045f5a7d49e64f1909f2cfe581cf2f05f
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-29 12:15:28 +0300

    feat: use HeroSection component for the JobDetails page

--- src/Components/Pages/JobDetails/JobDetails.jsx
+++ src/Components/Pages/JobDetails/JobDetails.jsx
@@ -1,9 +1,18 @@
+import HeroSection from "../../Shared/HeroSection/HeroSection";
 import s from "./JobDetails.module.scss";
 
 const JobDetails = () => {
   return (
     <main className={s.jobDetailsPage} id="jobDetailsPage">
-      JobDetails
+      <HeroSection
+        titleLine1="ELECTRICAL"
+        titleLine2="ENGINEER"
+        description={`I'm a paragraph. Click here to add your own text
+          and edit me. It’s easy. Just click “Edit Text” or double click
+          me to add your own content and make changes to the font.
+          I’m a great place for you to tell a story and let your users
+          know a little more about you.`}
+      />
     </main>
   );
 };
