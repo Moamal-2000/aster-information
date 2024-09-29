a4b2db75a23c46e39143de94fa440af3913743c0
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-29 11:40:51 +0300

    perf: alt description for TechHeroSection background image

--- src/Components/Pages/Technology/TechHeroSection/TechHeroSection.jsx
+++ src/Components/Pages/Technology/TechHeroSection/TechHeroSection.jsx
@@ -7,7 +7,7 @@ const TechHeroSection = () => {
       <img
         className={s.technologyBg}
         src={techPageBg}
-        alt="Technology Page Background"
+        alt="Geometric network of interconnected points forming a mesh-like structure on a white background."
       />
       <h1>INNOVATIVE DATA-DRIVEN SOLUTIONS</h1>
     </section>
