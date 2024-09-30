cf4d3293baf5ec2568cab4782616c0c73d4cb768
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-30 18:56:05 +0300

    delete: social media modules from images.js file

--- src/Assets/Images/Images.js
+++ src/Assets/Images/Images.js
@@ -1,22 +1,19 @@
+import adobeLogo from "./adobe-logo.webp";
 import amazonAwsLogo from "./amazon-aws-logo.webp";
 import coreExpertiseBg from "./core-expertise-section-bg.webp";
 import dataManagementBg from "./data-management-section-bg.webp";
 import docusignLogo from "./docusign-logo.webp";
-import facebookLogo from "./facebook.webp";
 import generalTransportLogo from "./general-transport.webp";
 import homeBgBottom from "./home-bg-bottom.webp";
 import homeBgTop from "./home-bg-top.webp";
 import idiSoftwareLogo from "./IDI-software.webp";
 import imogenCarsLogo from "./imogen-cars.webp";
 import infoSectionBg from "./info-section-bg.webp";
-import instagramLogo from "./instagram.webp";
 import joinOurTeamBg from "./join-our-team-bg.webp";
-import linkedinLogo from "./linkedin.webp";
 import locationSectionBg from "./location-section-bg.webp";
 import microsoftLogo from "./microsoft-logo.webp";
 import ourMissionSectionBg from "./our-mission-section-bg.webp";
 import paloAltotRibunal from "./palo-altot-ribunal.webp";
-import adobeLogo from "./adobe-logo.webp";
 import serviceSectionBg1 from "./services-bg1.webp";
 import serviceSectionBg2 from "./services-bg2.webp";
 import serviceSectionBg3 from "./services-bg3.webp";
@@ -24,30 +21,26 @@ import smallDataManagementBg from "./small-data-management-section-bg.webp";
 import teamCollaborationImg from "./team-collaboration.webp";
 import techPageBg from "./tech-bg.webp";
 import triNexLogo from "./tri-nex .webp";
-import twitterLogo from "./twitter.webp";
 import visionSectionBg from "./vision-section-bg.webp";
 import whyUsSectionBg from "./why-us-section-bg.webp";
 
 export {
+  adobeLogo,
   amazonAwsLogo,
   coreExpertiseBg,
   dataManagementBg,
   docusignLogo,
-  facebookLogo,
   generalTransportLogo,
   homeBgBottom,
   homeBgTop,
   idiSoftwareLogo,
   imogenCarsLogo,
   infoSectionBg,
-  instagramLogo,
   joinOurTeamBg,
-  linkedinLogo,
   locationSectionBg,
   microsoftLogo,
   ourMissionSectionBg,
   paloAltotRibunal,
-  adobeLogo,
   serviceSectionBg1,
   serviceSectionBg2,
   serviceSectionBg3,
@@ -55,7 +48,6 @@ export {
   teamCollaborationImg,
   techPageBg,
   triNexLogo,
-  twitterLogo,
   visionSectionBg,
   whyUsSectionBg,
 };
