a1fe0c51ff63d92fedb74420e24fd1b15bf246f4
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-09-30 07:52:07 +0300

    feat: animation fade in for StackedText paragraph

--- src/Components/Shared/MiniComponents/StackedText/StackedText.jsx
+++ src/Components/Shared/MiniComponents/StackedText/StackedText.jsx
@@ -1,12 +1,22 @@
+import { useRef } from "react";
+import useClassOnFirstView from "src/Hooks/App/useClassOnFirstView";
 import HorizontalLine from "../HorizontalLine/HorizontalLine";
 import s from "./StackedText.module.scss";
 
 const StackedText = ({ title, subTitle }) => {
+  const paragraphRef = useRef();
+  const activeClass = useClassOnFirstView({
+    elementRef: paragraphRef,
+    cssModule: s,
+  });
+
   return (
     <div className={s.wrapper}>
       <p>{title}</p>
       <HorizontalLine />
-      <p>{subTitle}</p>
+      <p className={activeClass} ref={paragraphRef}>
+        {subTitle}
+      </p>
     </div>
   );
 };
