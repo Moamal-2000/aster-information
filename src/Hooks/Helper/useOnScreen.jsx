214ad3d6f404d230cc3acbda08025cf230d72ceb
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-10-02 01:12:32 +0300

    perf: improve readability for useOnScreen hook

--- src/Hooks/Helper/useOnScreen.jsx
+++ src/Hooks/Helper/useOnScreen.jsx
@@ -1,23 +1,24 @@
 import { useEffect, useState } from "react";
 
-function useOnScreen(ref, options = { rootMargin: "0px", threshold: 1 }) {
+function useOnScreen(ref, { rootMargin = "0px", threshold = 1 } = {}) {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    if (!ref.current) return;
+    const currentRef = ref.current;
+    if (!currentRef) return;
 
     const observer = new IntersectionObserver(
       ([entry]) => setIsVisible(entry.isIntersecting),
-      options
+      { rootMargin, threshold }
     );
 
-    observer.observe(ref.current);
+    observer.observe(currentRef);
 
     return () => {
-      if (!ref.current) return;
-      observer.unobserve(ref.current);
+      if (!currentRef) return;
+      observer.unobserve(currentRef);
     };
-  }, [options]);
+  }, [ref, rootMargin, threshold]);
 
   return isVisible;
 }
