c7052b133566d7ccf4ec01a8153af0e1d5d77ba6
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-10-03 09:23:11 +0300

    feat: create & share global states to GlobalContext.Provider

--- src/Contexts/GlobalContext.js
+++ src/Contexts/GlobalContext.js
@@ -1,9 +1,21 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const GlobalContext = createContext();
 
 const GlobalContextProvider = ({ children }) => {
-  const state = {};
+  const [isMobileNavActive, setIsMobileNavActive] = useState(false);
+  const [isHeaderActive, setIsHeaderActive] = useState(false);
+  const [currentPage, setCurrentPage] = useState("/home");
+
+  const state = {
+    isMobileNavActive,
+    setIsMobileNavActive,
+    isHeaderActive,
+    setIsHeaderActive,
+    currentPage,
+    setCurrentPage,
+  };
+
   return (
     <GlobalContext.Provider value={state}>{children}</GlobalContext.Provider>
   );
