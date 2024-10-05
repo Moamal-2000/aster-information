88b0a232b2d1af64f2461cc5c985b74a2ba72250
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-10-05 05:16:54 +0300

    fix [edge browser]: hide AboutBackgroundImage on very top/bottom

--- src/Components/Pages/About/AboutBackgroundImage.jsx/AboutBackgroundImage.jsx
+++ src/Components/Pages/About/AboutBackgroundImage.jsx/AboutBackgroundImage.jsx
@@ -1,10 +1,13 @@
 import { teamCollaborationImg } from "src/Assets/Images/Images";
+import useScrollActiveClass from "src/Hooks/App/useScrollActiveClass";
 import s from "./AboutBackgroundImage.module.scss";
 
 const AboutBackgroundImage = () => {
+  const activeClass = useScrollActiveClass(s);
+
   return (
     <>
-      <div className={s.wrapper}>
+      <div className={`${s.wrapper} ${activeClass}`}>
         <img
           src={teamCollaborationImg}
           alt="Three individuals in an industrial setting: a man on the left speaking and gesturing, a girl in the middle holding a tablet, and a man on the right wearing a red and grey jacket."
