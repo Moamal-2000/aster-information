20cbafa36cdd26be0d85648da81b02fe0cbdef40
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-10-06 11:51:52 +0300

    style: fix background-image for Home & About pages on screens 1540px and above

--- src/Components/Pages/About/AboutBackgroundImage.jsx/AboutBackgroundImage.jsx
+++ src/Components/Pages/About/AboutBackgroundImage.jsx/AboutBackgroundImage.jsx
@@ -1,13 +1,10 @@
 import { teamCollaborationImg } from "src/Assets/Images/Images";
-import useScrollActiveClass from "src/Hooks/App/useScrollActiveClass";
 import s from "./AboutBackgroundImage.module.scss";
 
 const AboutBackgroundImage = () => {
-  const activeClass = useScrollActiveClass(s);
-
   return (
     <>
-      <div className={`${s.wrapper} ${activeClass}`}>
+      <div className={s.wrapper}>
         <img
           src={teamCollaborationImg}
           alt="Three individuals in an industrial setting: a man on the left speaking and gesturing, a girl in the middle holding a tablet, and a man on the right wearing a red and grey jacket."
