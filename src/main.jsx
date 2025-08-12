a7d570fa4e62406ff5132556f26122d230e45cc2
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-12 17:27:48 +0300

    update(style): rename file index.scss with global.scss

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import GlobalContextProvider from "./Context/GlobalContext.jsx";
-import "./Styles/index.scss";
+import "./Styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
