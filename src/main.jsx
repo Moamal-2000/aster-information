d2b8f6fea01a75b9be9af2eb5c9e45b2b773f023
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-12 18:32:49 +0300

    fix(main.jsx): correct global.scss import path

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import GlobalContextProvider from "./Context/GlobalContext.jsx";
-import "./Styles/global.scss";
+import "./Styles/_global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
